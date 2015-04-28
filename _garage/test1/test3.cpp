fc9838f8e161c8ae3d782066d095ddb5394da921
Maxim <maxim@mxmz.net>
2015-04-28 21:42:20 +0200

    asio example in test1

--- _garage/test1/test3.cpp
+++ _garage/test1/test3.cpp
@@ -5,10 +5,14 @@
 #include <string>
 #include <map>
 #include <functional>
+#include <memory>
+
+#include <boost/asio.hpp>
 
 #include "http_parser.hxx"
 
 using namespace std;
+using namespace boost::asio;
 
 
 template < class Derived >
@@ -17,7 +21,9 @@ class base_handlers : public  mxmz::http_parser_base<Derived> {
     map< string, string > headers;
 
 public:
-    base_handlers() : mxmz::http_parser_base<Derived>( mxmz::http_parser_base<Derived>::Request) {}
+    base_handlers() :
+        mxmz::http_parser_base<Derived>( mxmz::http_parser_base<Derived>::Request)
+    {}
 
     void  on_body(const char* b, size_t l) {
         cerr << "body data: " << l << " ";
@@ -64,28 +70,107 @@ void dump( const Map& m ) {
 
 
 
-class my_handlers : public base_handlers<my_handlers> {
+
+/* --------------------------------------------- --------------------------------------------- --------------------------------------------- */
+
+struct request {
+    string method;
+    string url;
+    map<string,string> headers;
+    string body;
+    bool ready;
+
+    request( string method, string url, map<string,string>&& headers ) :
+        method(move(method) ),
+        url( move(url) ),
+        headers( move(headers) ),
+        ready(false)
+    {
+    }
+};
+
+class http_connection : public base_handlers<http_connection>,  public std::enable_shared_from_this<http_connection> {
+    ip::tcp::socket socket;
+    size_t          bytes_in_body;
+    unique_ptr<request>     current_request;
 public:
+    http_connection( ip::tcp::socket&& s ) :
+        socket( move(s) ), bytes_in_body(0)
+    {}
 
-    using base_handlers<my_handlers>::on_request_headers_complete;
-    using base_handlers<my_handlers>::on_response_headers_complete;
+    using base_handlers<http_connection>::on_request_headers_complete;
+    using base_handlers<http_connection>::on_response_headers_complete;
 
-    void on_response_headers_complete( int code, const string& response_status, map<string,string>&& handlers ) {
+    void on_response_headers_complete( int code, const string& response_status, map<string,string>&& headers ) {
         cerr << "res" << endl;
-        dump(handlers);
+        dump(headers);
     }
 
-    void  on_request_headers_complete( const string& method, const string& request_url, map<string,string>&& handlers ) {
+    void  on_request_headers_complete( const string& method, const string& request_url, map<string,string>&& headers ) {
         cerr << "req" << endl;
-        dump(handlers);
+        current_request.reset(  new request( method, request_url, move(headers) ) );
+        dump(headers);
     };
 
     void on_message_complete()
     {
 //        pause();
         cerr << "Message complete (derived)" << endl;
+        current_request->ready = true;
     }
 
+    void  on_body(const char* b, size_t l) {
+        cerr << "body data: " << l << " ";
+        //cerr.write(b, l) << endl;
+        current_request->body.append( b, l );
+    }
+
+    std::array<char, 8192> buffer;
+    string response_buffer;
+
+    void start_read() {
+        auto self(shared_from_this());
+
+        socket.async_read_some(boost::asio::buffer(buffer),
+                               [this,self](boost::system::error_code ec, std::size_t bytes_transferred)
+        {
+            if (!ec)
+            {
+                size_t read = parse( buffer.data(), bytes_transferred );
+                if ( read != bytes_transferred ) {
+                    cerr << "Error" << endl;
+                }
+                if ( current_request ) {
+                    if ( current_request->ready ) {
+                        cerr << "request and body ready" << endl;
+
+                        response_buffer =   "HTTP/1.0 200 OK\r\n"
+                                            "Content-Length: 4\r\n"
+                                            "Content-Type: text/plain\r\n"
+                                            "\r\n"
+                                            "OK\r\n";
+                        async_write(  socket, boost::asio::buffer(response_buffer),
+                        [this,self](boost::system::error_code ec, std::size_t) {
+                            boost::system::error_code ignored_ec;
+                            socket.shutdown(ip::tcp::socket::shutdown_both, ignored_ec);
+                        } );
+
+                    } else {
+                        cerr << "body not ready yet" << endl;
+                        start_read();
+                    }
+
+                } else {
+                    cerr << "request not ready yet" << endl;
+                    start_read();
+                }
+            }
+
+
+        }
+                              );
+
+    }
 
 };
 
@@ -93,20 +178,42 @@ public:
 
 int main()
 {
-    my_handlers parser;
 
-    const string s1 = "POST /post_identity_body_world?q=search#hey HTTP/1.1\r\n"
-                      "Accept: */*\r\n"
-                      "Transfer-Encoding: identity\r\n"
-                      "Content-Length: 5\r\n"
-                      "\r\n"
-                      "World";
+    io_service  ios;
+
+    ip::tcp::resolver resolver(ios);
+    ip::tcp::endpoint endpoint = *resolver.resolve({"localhost", "2080"});
+
+    ip::tcp::acceptor acceptor(ios);
+
+    ip::tcp::socket socket(ios);
 
 
-    for( auto& c : s1 ) {
-        parser.parse(&c, 1 );
-    }
+    acceptor.open(endpoint.protocol());
+    acceptor.set_option(ip::tcp::acceptor::reuse_address(true));
+    acceptor.bind(endpoint);
+    acceptor.listen();
 
+    function<void()> start_accept = [&acceptor,&socket,&start_accept]() {
+        acceptor.async_accept( socket, [&acceptor,&socket,&start_accept](boost::system::error_code ec)
+        {
+            if (!acceptor.is_open())
+            {
+                return;
+            }
+
+            if (!ec)
+            {
+                cerr << "new connnection" << endl;
+                auto conn = make_shared<http_connection>( move(socket));
+                conn->start_read();
+            }
+            start_accept();
+        } );
+    };
+
+    start_accept();
+    ios.run();
 }
 
 #include "detail/http_parser_impl.hxx"
