c027ac34ad6006c9272d27d8b073b0f7da30ae51
Max Muzi <max.muzi@kpnqwest.it>
2016-09-08 18:40:11 +0200

    fix

--- samples/ring_buffer_test.cpp
+++ samples/ring_buffer_test.cpp
@@ -19,6 +19,7 @@ using namespace std;
 
 std::string make_random_string(int c) {
     std::string s;
+    s.reserve(c);
     for ( int i = 0; i < c; ++i) {
         s.push_back( 'a' + rand() % 26 );
     }
@@ -32,29 +33,32 @@ void random_test(size_t source_base_size) ;
 
 int main()
 {
-    size_t source_base_size  = 1000;
+    size_t source_base_size  = 1000 + ( rand() % 1000);
     srand (time(nullptr));
     auto now = time(nullptr);
-    while ( time(nullptr) - now < 4) {
+    while ( (time(nullptr) - now < 10 ) or (source_base_size < 50000000) ) {
         random_test(source_base_size );
-        source_base_size *= ( 1 + rand() % 10 );
+        source_base_size *= ( 1 + rand() % 4 ) ;
+        source_base_size += ( rand() % 1000 ) ;
     }
 }
 
 void random_test(size_t source_base_size ) {
    
 
-    mxmz::ring_buffer   rb(1024 + rand() % 1000);
+    cerr << "preparing... size=" << source_base_size << endl;
+    mxmz::ring_buffer   rb(1024 + rand() % (source_base_size/10));
     auto source = make_random_string(source_base_size  + rand() % 1000);
 //    cerr << source << endl;
 
+    cerr << "testing..." << endl;
     cerr << rb.writable() << endl;
     cerr << source.size() <<endl;
 
     using boost::asio::buffer_cast;
 
     std::thread t1( [&source,&rb]{
-            
+            return;
             auto  i = source.begin();
             while ( i != source.end() ) {
                 if ( not rb.full() ) {
@@ -65,7 +69,7 @@ void random_test(size_t source_base_size ) {
                     rb.commit(chunk);
                     i += chunk;
                 } else {
-                    cerr << "sleeping ...  source read = " << (i - source.begin() )  << endl;
+   //                 cerr << "sleeping ...  source read = " << (i - source.begin() )  << endl;
                     std::this_thread::sleep_for( std::chrono::milliseconds(rand() % 10 ));
                 }
             } 
@@ -74,6 +78,7 @@ void random_test(size_t source_base_size ) {
 
     std::string copy;
     std::thread t2( [&rb, &copy, expected = source.size() ]{
+        return;
         while ( copy.size() < expected ) {
             if ( not rb.empty() ) {
                 auto buff = *rb.data().begin();
@@ -82,7 +87,7 @@ void random_test(size_t source_base_size ) {
                 copy.append(data,size);
                 rb.consume(size);
             } else {
-                cerr << "sleeping ...  copy written = " << copy.size()  << endl;
+ //               cerr << "sleeping ...  copy written = " << copy.size()  << endl;
                 std::this_thread::sleep_for( std::chrono::milliseconds(rand() % 10 ));
             }
 
@@ -92,8 +97,9 @@ void random_test(size_t source_base_size ) {
 
     t1.join();
     t2.join();
-    
+    cerr << "checking..." << endl;   
     assert( source == copy );
+    cerr << "ok" << endl;
     
 }
 
